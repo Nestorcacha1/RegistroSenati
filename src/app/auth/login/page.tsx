c25a71d66c2a2e7ca15a44da46c3225ddc79055b
NestorDev <edicelmi14@gmail.com>
2024-11-05 19:25:23 -0500

    Se corrigio la login

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -42,38 +42,21 @@ function LoginPage() {
 	if (status === 'unauthenticated') {
 		return (
 			<div className='flex flex-col items-center justify-center min-h-screen bg-blue-300'>
-				<div className='flex flex-col items-center justify-center p-6 space-y-6 bg-blue-800 rounded-lg shadow-lg sm:w-3/4 md:w-1/2 lg:w-1/3'>
-					<h2 className='text-xl font-bold text-center text-white sm:text-2xl md:text-3xl'>
+				<div className='flex flex-col items-center justify-center p-6 space-y-6 bg-white rounded-lg shadow-lg sm:w-3/4 md:w-1/2 lg:w-1/3'>
+					<h2 className='text-xl font-bold text-center text-gray-800 sm:text-2xl md:text-3xl'>
 						Inicio De Sesión Del Administrador
 					</h2>
-
-					<DownIcon className='w-12 h-12 text-blue-500 font-bold animate-bounce' />
-
 					<button
 						onClick={handleGoogleSignIn}
-						className='bg-white border border-gray-300 hover:bg-blue-700 hover:text-white delay-75 focus:outline-none rounded-xl flex items-center justify-center p-3 space-x-2 shadow-md transition-transform transform hover:scale-105'
+						className='px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600'
 					>
-						<Image
-							src='/google.png'
-							alt='google'
-							width={24}
-							height={24}
-							className='w-6 h-6'
-						/>
-						<span className='text-gray-700 font-medium group-hover:text-white hover:text-white'>
-							Sign in with Google
-						</span>
+						Iniciar sesión con Google
 					</button>
-
-					<span className='text-center text-sm text-white sm:text-base'>
-						Inicia sesión con Google
-					</span>
 				</div>
 			</div>
 		)
 	}
 
-	// No renderizar nada si está autenticado (el useEffect se encargará de la redirección)
 	return null
 }
 
